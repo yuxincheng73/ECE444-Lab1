70e9a5ff94ee85148a1aa5c0224b255fe22fd7d1
Yu Xin Cheng <yx.cheng73@gmail.com>
2021-09-16 14:56:59 -0400

    Commit 3

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,9 @@
 if __name__ == '__main__':
     print("Hello World - been at UofT for 4 years")
     print("Nice to meet you !")
+
+    ####
+    #avoiding merge conflicts
+    ####
+
+    print("commit 3")
