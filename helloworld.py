cb4aa08336217b3a6f34ae06df1cf739f5dc723a
Yu Xin Cheng <yx.cheng73@gmail.com>
2021-09-19 19:16:23 -0400

    Revert "Commit 3"
    This reverts commit 70e9a5ff94ee85148a1aa5c0224b255fe22fd7d1.

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,3 @@
 if __name__ == '__main__':
     print("Hello World - been at UofT for 4 years")
     print("Nice to meet you !")
-
-    ####
-    #avoiding merge conflicts
-    ####
-
-    print("commit 3")
